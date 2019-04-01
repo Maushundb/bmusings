c0adb24d8ce2ed14e56a64417bcf4691d2f2550b
Brandon Maushund <maushundb@gmail.com>
2019-04-01 10:37:03 -0700

    Link headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,12 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              className: 'linkAnchor',
+            },
+          },
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
